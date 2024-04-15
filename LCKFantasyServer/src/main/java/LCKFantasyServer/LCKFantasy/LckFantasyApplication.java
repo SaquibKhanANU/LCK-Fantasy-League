2da443b7c6e60c0ed5fbc87fef739a9a375af08c
U7108513 <u7108513@anu.edu.au>
2024-04-15 14:26:34 +1000

    Initial commit

---
+++ LCKFantasyServer/src/main/java/LCKFantasyServer/LCKFantasy/LckFantasyApplication.java
@@ -0,0 +1,13 @@
+package LCKFantasyServer.LCKFantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LckFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LckFantasyApplication.class, args);
+	}
+
+}
---
+++ LCKFantasyServer/src/test/java/LCKFantasyServer/LCKFantasy/LckFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package LCKFantasyServer.LCKFantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LckFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
